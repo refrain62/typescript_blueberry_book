477dc3d9dc71c05b761bb64dc2e840edb6594a4d
refrain62 <nao_h13@hotmail.com>
2023-07-14 16:39:54 +0900

    2.3.2

---
+++ practice/src/2.3.2/index.ts
@@ -0,0 +1,12 @@
+{
+    const width1 = 5;
+    const width2 = 8;
+    const height: number = 3;
+    const area = (width1 + width2) * height / 2;
+    
+    // 19.5
+    console.log(area);
+
+    // 7.0ではなく7(number型では整数と少数の区別がない)
+    console.log(3.5 * 2);
+}
