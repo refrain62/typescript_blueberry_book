5e2dda0d71298150e145f6d226450b82c9664834
refrain62 <nao_h13@hotmail.com>
2023-07-28 22:58:02 +0900

    2.3.9

---
+++ practice/src/2.3.9.ts
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('文字列を入力してください? ', (answer) => {
+  // 文字列が入力されるとここが実行される
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  // 1000を足して出力
+  // ⇒ 1231000 と表示さえる
+  console.log(answer + 1000);
+
+  // いったん変数に入れて出力
+  // ⇒ それでも文字列として処理される 暗黙の変換
+  const result = answer + 1000;
+  console.log(result);
+
+  rl.close();
+});
