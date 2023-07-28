a320e50ae79fc12a0c0e47845756607e0c7572b6
refrain62 <nao_h13@hotmail.com>
2023-07-28 22:35:58 +0900

    2.3.8

---
+++ practice/src/2.3.8.ts
@@ -0,0 +1,13 @@
+{
+    const val1 = null;
+    const val2 = undefined;
+
+    // null undefined と表示される
+    console.log(val1, val2);
+}
+{
+    const n: null = null;
+    const u: undefined = undefined;
+
+    console.log(n, u);
+}
